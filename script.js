f9afed61bc07c70cd44eef71ef36dab95b3f18a6
insidejob1 <aritrarouth1@gmail.com>
2024-08-27 00:06:07 +0530

    demo link added

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+//to disable right click and inspect element
+// document.addEventListener("contextmenu", function (event) {
+//   event.preventDefault();
+// });
+
+// //to disable F12 key,specific keyboard shortcuts
+// document.addEventListener("keydown", function (event) {
+//   // Prevent F12, Ctrl+U, Ctrl+Shift+I, Ctrl+Shift+C
+//   if (event.key === "F12" || 
+//       (event.ctrlKey && event.key === "u") || 
+//       (event.ctrlKey && event.shiftKey && event.key === "I") || 
+//       (event.ctrlKey && event.shiftKey && event.key === "C")) {
+//     event.preventDefault();
+//   }
+// });
 
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
